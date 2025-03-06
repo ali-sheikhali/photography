6eb2d5c4de3af15021df1a5908692f72523de46f
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-03-06 09:42:39 +0330

    Implement comment and suggestion

--- src/components/home/CommentsSuggestions.tsx
+++ src/components/home/CommentsSuggestions.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import SubTitle from './SubTitle'
-import SuggestForm from './SuggestForm'
+import React from "react";
+import SubTitle from "./SubTitle";
+import SuggestForm from "./SuggestForm";
+import lenz from "../../assets/lenz.jpg";
 
 function CommentsSuggestions() {
   return (
-    <div>
-        <SubTitle title='نظر و پیشنهاد' />
-        <div>
-            <SuggestForm />
+    <div className="flex flex-col gap-8">
+      <SubTitle title="نظر و پیشنهاد" />
+      
+      <div className="relative flex flex-col gap-8 py-5 rounded-md overflow-hidden">
+        <div className="absolute inset-0">
+          <img 
+            src={lenz} 
+            alt="Background" 
+            className="w-full h-full object-cover opacity-5"
+          />
         </div>
+        <div className="relative z-10">
+          <SuggestForm />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CommentsSuggestions
+export default CommentsSuggestions;
--- src/components/home/SuggestForm.tsx
+++ src/components/home/SuggestForm.tsx
@@ -1,65 +1,85 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import FormFiled from "../FormFiled";
 import FormError from "../FormError";
-import arrowDown from "../../assets/arrow-down.svg"
+import arrowDown from "../../assets/arrow-down.svg";
+
 interface FormValue {
   name: string;
+  description: string;
+  photographer: string;
 }
+
 const SuggestForm = () => {
   const validationSchema = Yup.object({
     name: Yup.string().required("لطفا نام و نام خانوادگی را وارد کنید."),
+    description: Yup.string().required("لطفا نظر خود را وارد کنید."),
+    photographer: Yup.string().required("لطفا یک عکاس را انتخاب کنید."),
   });
+
   const formik = useFormik<FormValue>({
     initialValues: {
       name: "",
+      description: "",
+      photographer: "",
     },
     validationSchema,
-    onSubmit: () => {},
+    onSubmit: (values) => {
+      console.log("Form Submitted! Values:", values);
+    },
   });
+
   return (
     <form
       className="flex flex-col gap-5 w-11/12 lg:w-8/12 lg:flex lg:justify-center mx-auto"
       onSubmit={formik.handleSubmit}
     >
       <div className="flex flex-col gap-5">
-        <FormFiled
+        <input
           type="text"
           name="name"
-          placeHolder="نام و نام خانوادگی خود را وارد کنید."
+          placeholder="نام و نام خانوادگی خود را وارد کنید."
+          value={formik.values.name}
+          onChange={formik.handleChange}
           onBlur={formik.handleBlur}
-          label=""
-          borderColor="border-[#247D7B]"
+          className="border-[#247D7B] border py-2 px-3 focus:outline-none placeholder:text-[#737373] rounded-md"
         />
         <FormError title="name" formik={formik} />
         <div className="relative w-full">
-    <select
-      defaultValue="Medium"
-      className="appearance-none w-full focus:outline-none text-white border border-[#247D7B] py-2 px-4  rounded-md bg-transparent"
-    >
-      <option disabled={true} className="text-[#737373]">عکاس خود را انتخاب کنید</option>
-      <option className="text-black">علی احمدی</option>
-      <option className="text-black">مریم رضایی</option>
-      <option className="text-black">سارا موسوی</option>
-    </select>
-    <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-      <img src={arrowDown} alt="" />
-    </div>
-</div>
-        <FormError title="password" formik={formik} />
+          <select
+            name="photographer"
+            value={formik.values.photographer}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`appearance-none w-full focus:outline-none border border-[#247D7B] py-2 px-4 rounded-md bg-transparent 
+            ${formik.values.photographer ? "text-white" : "text-[#737373]"}`}
+          >
+            <option value="" disabled className="text-[#737373]">
+              عکاس خود را انتخاب کنید
+            </option>
+            <option value="علی احمدی">علی احمدی</option>
+            <option value="مریم رضایی">مریم رضایی</option>
+            <option value="سارا موسوی">سارا موسوی</option>
+          </select>
+          <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
+            <img src={arrowDown} alt="" />
+          </div>
+        </div>
+        <FormError title="photographer" formik={formik} />
         <textarea
-          className="h-[128px] resize-none border border-[#247D7B] placeholder:text-[#737373] rounded-md px-1 py-2"
+          className="h-[128px] resize-none focus:outline-none text-white font-bold border border-[#247D7B] placeholder:text-[#737373] rounded-md px-1 py-2"
           name="description"
-          id=""
+          value={formik.values.description}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           placeholder="نظرات و پیشنهادات و یا تجربیات خود را برای ما ارسال کنید."
         ></textarea>
+        <FormError title="description" formik={formik} />
       </div>
       <div className="flex justify-end">
-
-      <button className="buttonOfSuggest" type="submit">
-        ارسال
-      </button>
+        <button className="buttonOfSuggest" type="submit">
+          ارسال
+        </button>
       </div>
     </form>
   );
