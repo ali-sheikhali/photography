65234d1b5099b276dca5ef9477788773507ba52e
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-03-07 00:12:43 +0330

    Implement price list

---
+++ src/components/PriceList.tsx
@@ -0,0 +1,41 @@
+// import React from 'react'
+
+// const PriceList = () => {
+//     const handleClick = () => {
+//         setOpenModal(false);
+//       };
+//   return (
+//     <div>
+//         <div className="flex flex-col  mx-auto py-8 backdrop-blur-2xl bg-[#171717] rounded-xl">
+//       <div className="flex items-center w-11/12 mx-auto justify-between">
+//         <p>لیست قیمت</p>
+//         <img className="cursor-pointer" onClick={handleClick} src={closeSquare} alt="close-square" />
+//       </div>
+//       <div className="w-11/12 mx-auto flex flex-col gap-5 mt-12">
+//         <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+//           <p>عکاسی در طبیعت</p>
+//           <p>هرشات ۱۰۰.۰۰۰ تومان</p>
+//         </div>
+//         <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+//           <p>عکاسی در طبیعت</p>
+//           <p>هرشات ۱۰۰.۰۰۰ تومان</p>
+//         </div>
+//         <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+//           <p>عکاسی در طبیعت</p>
+//           <p>هرشات ۱۰۰.۰۰۰ تومان</p>
+//         </div>
+//         <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+//           <p>عکاسی در طبیعت</p>
+//           <p>هرشات ۱۰۰.۰۰۰ تومان</p>
+//         </div>
+//         <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+//           <p>عکاسی در طبیعت</p>
+//           <p>هرشات ۱۰۰.۰۰۰ تومان</p>
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default PriceList
--- src/components/home/NavBarHome.tsx
+++ src/components/home/NavBarHome.tsx
@@ -1,13 +1,92 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import logo from "../../assets/logo.png";
+import menu from "../../assets/menu.svg";
+import instagram1 from "../../assets/instagram1.svg";
+import CostModal from "../CostModal";
+import BottomSheet from "@wldyslw/react-bottom-sheet"; // Import BottomSheet
 
 function NavBarHome() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const bottomSheetRef = useRef<any>(null);
+
+  const handleClick = () => {
+    console.log("log log");
+
+    setOpenMenu(false);
+    if (bottomSheetRef.current) {
+      bottomSheetRef.current.open();
+    }
+  };
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setOpenMenu(false);
+      }
+    };
+
+    if (openMenu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [openMenu]);
+
   return (
-    <div className='w-full bg-[#1D1818] text-[#FAFAFA] py-7 z-10 border-b border-[#247D7B]'>
-        <div className='w-11/12 mx-auto'>
-            fsgsfdgs
+    <div className="w-full bg-[#1D1818] text-[#FAFAFA] py-2 z-10 border-b border-[#247D7B] relative">
+      <div className="w-11/12 mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={logo} alt="logo" />
+          <button className="sm:hidden" onClick={() => setOpenMenu(true)}>
+            <img src={menu} alt="menu" />
+          </button>
+          <div className="gap-4 hidden sm:flex">
+            <p>نمونه کارها</p>
+            <p className="cursor-pointer" onClick={() => setOpenModal(true)}>
+              لیست قیمت
+            </p>
+            <p>تماس با ما</p>
+            <p>بلاگ</p>
+          </div>
         </div>
+        <div className="flex items-center justify-center gap-2">
+          <p>kokoro.official</p>
+          <img src={instagram1} alt="instagram" />
+        </div>
+      </div>
+      <div
+        ref={menuRef}
+        className={`w-6/12 h-screen absolute top-0 right-0 bg-[#1D1818] text-white transform transition-transform duration-300 ${
+          openMenu ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="w-10/12 mx-auto flex flex-col gap-4 py-6">
+          <p>نمونه کارها</p>
+          <p onClick={handleClick}>لیست قیمت</p>
+          <p>تماس با ما</p>
+          <p>بلاگ</p>
+        </div>
+      </div>
+      {openModal && (
+        <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50">
+          <div className="inset-0 absolute backdrop-blur-sm"></div>
+          <div className="relative w-6/12  rounded-lg py-8 px-4 ">
+            <CostModal setOpenModal={setOpenModal} rounded={false} />
+          </div>
+        </div>
+      )}
+      <BottomSheet
+        detents={["70%"]}
+        ref={bottomSheetRef}
+        className="bg-black h-full text-white"
+      >
+        <CostModal setOpenModal={setOpenModal} rounded={true} />
+      </BottomSheet>
     </div>
-  )
+  );
 }
 
-export default NavBarHome
+export default NavBarHome;
