2cde169af5990995dce9b18a1713f3e1ba85e1c6
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-03-31 20:21:41 +0330

    add auth services

---
+++ src/services/authServices.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = "https://kokoro.liara.run/api/admin/login";
+
+ export const loginUser = async (email: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_URL}`, {
+      username: email,
+      password,
+    });
+    
+    if (response.data.token) {
+      localStorage.setItem("token", response.data.token); 
+    }
+
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || "Login failed");
+  }
+};
+export const logoutUser = () => {
+  localStorage.removeItem("token");
+  window.location.href = "/login";
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
