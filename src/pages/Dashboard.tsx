e41e3dab8f7343aa1ad5291edf8aeb899ec212c9
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-04-04 04:49:28 +0330

    modify home page and dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -19,38 +19,41 @@ function Dashboard() {
     {id:2 , title:"photo" , name:"عکس"},
     {id:3 , title:"blog" , name:"بلاگ"},
     {id:4 , title:"about" , name:"درباره ما"},
-
   ]
   const activeData = data.find((item) => item.title === activeTab);
+
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-[#1D1818] text-white">
       <NavBar />
-      <main className="bg-[#1D1818] text-white w-full ">
-        <div className="w-11/12 mx-auto flex flex-col  py-4">
-           <div className="flex gap-4 my-12 border-b border-[#292524] py-1">
-          {data.map((item, index) => (
-          <div key={index}>
-            <button
-              className={`px-6 py-2 ${
-                activeTab === item.title
-                  ? "bg-[#247D7B] text-white rounded-xl"
-                  : ""
-              }`}
-              onClick={() => setActiveTab(item.title)}
-            >
-              {item.name}
-            </button>
+      
+      <main className="w-full flex-grow">
+        <div className="w-11/12 mx-auto flex flex-col py-4">
+          <div className="flex gap-4 my-12 border-b border-[#292524] py-1">
+            {data.map((item, index) => (
+              <div key={index}>
+                <button
+                  className={`px-6 py-2 ${
+                    activeTab === item.title
+                      ? "bg-[#247D7B] text-white rounded-xl"
+                      : ""
+                  }`}
+                  onClick={() => setActiveTab(item.title)}
+                >
+                  {item.name}
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
-          </div>
+
           {activeData?.title === "photographer" && <AddPhotographer /> }
           {activeData?.title === "photo" && <AddPhoto /> }
           {activeData?.title === "blog" && <AddBlog /> }
           {activeData?.title === "about" && <AddAbout /> }
         </div>
       </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/routs/routs.tsx
+++ src/routs/routs.tsx
@@ -2,33 +2,32 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import ForgetPassword from "../pages/ForgetPassword";
-import Dashboard from "../pages/Dashboard"
+import Dashboard from "../pages/Dashboard";
 import Home from "../pages/Home";
 import Details from "../pages/Details";
 import Blogs from "../pages/Blogs";
 import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children:[
-            {path: "/login" , element:<Login />},
-            {path: "/forget-password" , element:<ForgetPassword />},
-            {
-                element: <PrivateRoute />,
-                children: [
-                  { path: "/dashboard", element: <Dashboard /> },
-                ],
-              },            {path: "/home" , element:<Home />},
-            {path: "/details" , element:<Details />},
-            {path: "/blogs" , element:<Blogs />},
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: "true", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/forget-password", element: <ForgetPassword /> },
+      {
+        element: <PrivateRoute />,
+        children: [{ path: "/dashboard", element: <Dashboard /> }],
+      },
+      { path: "/details", element: <Details /> },
+      { path: "/blogs", element: <Blogs /> },
+    ],
+  },
+]);
 
-const Routes : React.FC = ()=>{
-    return <RouterProvider router={router} />
-}
+const Routes: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
-export default Routes
+export default Routes;
