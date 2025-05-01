3d6aa6b6c0142a41a4ea499ceb31397362f474d0
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-05-01 14:25:20 +0330

    fix logo and cost

--- src/components/CostModal.tsx
+++ src/components/CostModal.tsx
@@ -3,40 +3,49 @@ interface CostModalProps {
   setOpenModal: (value: boolean) => void; // Function that updates state
   rounded?: boolean; // Optional boolean prop
 }
-const CostModal = ({ setOpenModal , rounded }:CostModalProps) => {
+const CostModal = ({ setOpenModal, rounded }: CostModalProps) => {
   const handleClick = () => {
     setOpenModal(false);
   };
   return (
     <>
-    <div className={`flex flex-col h-full mx-auto py-8 backdrop-blur-2xl bg-[#171717] ${rounded ? "rounded-none" : "rounded-2xl" } `}>
-      <div className="flex items-center w-11/12 mx-auto justify-between">
-        <p>لیست قیمت</p>
-        {/* <img className="cursor-pointer" onClick={handleClick} src={closeSquare} alt="close-square" /> */}
+      <div
+        className={`flex flex-col h-full mx-auto py-8 backdrop-blur-2xl bg-[#171717] ${
+          rounded ? "rounded-none" : "rounded-2xl"
+        } `}
+      >
+        <div className="flex items-center w-11/12 mx-auto justify-between">
+          <p>لیست قیمت</p>
+          <img
+            className="cursor-pointer"
+            onClick={handleClick}
+            src={closeSquare}
+            alt="close-square"
+          />
+        </div>
+        <div className="w-11/12 mx-auto flex flex-col gap-5 mt-12">
+          <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+            <p>عکاسی طبیعت</p>
+            <p>تماس بگیرید</p>
+          </div>
+          <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+            <p>عکاسی خیابانی</p>
+            <p>تماس بگیرید</p>
+          </div>
+          <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+            <p>عکاسی انتزاعی</p>
+            <p>تماس بگیرید</p>
+          </div>
+          <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+            <p>عکاسی نور</p>
+            <p>تماس بگیرید</p>
+          </div>
+          <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
+            <p>عکاسی مستند</p>
+            <p>تماس بگیرید</p>
+          </div>
+        </div>
       </div>
-      <div className="w-11/12 mx-auto flex flex-col gap-5 mt-12">
-        <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
-          <p>عکاسی در طبیعت</p>
-          <p>هرشات ۱۰۰.۰۰۰ تومان</p>
-        </div>
-        <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
-          <p>عکاسی در طبیعت</p>
-          <p>هرشات ۱۰۰.۰۰۰ تومان</p>
-        </div>
-        <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
-          <p>عکاسی در طبیعت</p>
-          <p>هرشات ۱۰۰.۰۰۰ تومان</p>
-        </div>
-        <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
-          <p>عکاسی در طبیعت</p>
-          <p>هرشات ۱۰۰.۰۰۰ تومان</p>
-        </div>
-        <div className="w-full py-2 flex justify-between items-center border-b border-[#247D7B]">
-          <p>عکاسی در طبیعت</p>
-          <p>هرشات ۱۰۰.۰۰۰ تومان</p>
-        </div>
-      </div>
-    </div>
     </>
   );
 };
--- src/components/home/NavBarHome.tsx
+++ src/components/home/NavBarHome.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.jpg";
 import menu from "../../assets/menu.svg";
 import instagram1 from "../../assets/instagram1.svg";
 import CostModal from "../CostModal";
@@ -17,12 +17,12 @@ interface BottomSheetRef {
   open: () => void;
   close?: () => void;
 }
-function NavBarHome({ scrollToSection , notNeedMenu }: Services) {
+function NavBarHome({ scrollToSection, notNeedMenu }: Services) {
   const [openMenu, setOpenMenu] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
   const bottomSheetRef = useRef<BottomSheetRef | null>(null);
-  
+
   const handleClick = () => {
     setOpenMenu(false);
     if (bottomSheetRef.current) {
@@ -49,31 +49,40 @@ function NavBarHome({ scrollToSection , notNeedMenu }: Services) {
     <div className="w-full bg-[#1D1818] text-[#FAFAFA] py-2 z-10 border-b border-[#247D7B] relative">
       <div className="w-11/12 mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          {!notNeedMenu && 
-          <button className="sm:hidden" onClick={() => setOpenMenu(true)}>
-            <img src={menu} alt="menu" />
-          </button>
-          }
+          {!notNeedMenu && (
+            <button className="sm:hidden" onClick={() => setOpenMenu(true)}>
+              <img src={menu} alt="menu" />
+            </button>
+          )}
           <Link to="/">
-            <img src={logo} alt="logo" />
+            <img
+              src={logo}
+              alt="logo"
+              className=" border-none w-16 h-16 sm:w-22 sm:h-22 object-contain"
+            />
           </Link>
-          {!notNeedMenu && 
-          <div className="gap-4 hidden sm:flex">
-            <p
-              className="cursor-pointer"
-              onClick={() => scrollToSection?.work()}
+          {!notNeedMenu && (
+            <div className="gap-4 hidden sm:flex">
+              <p
+                className="cursor-pointer"
+                onClick={() => scrollToSection?.work()}
               >
-              نمونه کارها
-            </p>
-            <p className="cursor-pointer" onClick={() => setOpenModal(true)}>
-              لیست قیمت
-            </p>
-            <p className="cursor-pointer" onClick={() => scrollToSection?.footer()}>تماس با ما</p>
-            <Link to="/blogs">
-              <p>بلاگ</p>
-            </Link>
-          </div>
-            }
+                نمونه کارها
+              </p>
+              <p className="cursor-pointer" onClick={() => setOpenModal(true)}>
+                لیست قیمت
+              </p>
+              <p
+                className="cursor-pointer"
+                onClick={() => scrollToSection?.footer()}
+              >
+                تماس با ما
+              </p>
+              <Link to="/blogs">
+                <p>بلاگ</p>
+              </Link>
+            </div>
+          )}
         </div>
         <a
           href="https://www.instagram.com/Kokoro__photography"
@@ -86,41 +95,40 @@ function NavBarHome({ scrollToSection , notNeedMenu }: Services) {
           </div>
         </a>
       </div>
-      {!notNeedMenu && 
-    
-      <div
-        ref={menuRef}
-        className={`w-6/12 h-screen absolute top-0 right-0 bg-[#1D1818] text-white transform transition-transform duration-300 ${
-          openMenu ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <div className="w-10/12 mx-auto flex flex-col gap-4 py-6">
-          <p
-            onClick={() => {
-              scrollToSection?.work();
-              setOpenMenu(false);
-            }}
-          >
-            نمونه کارها
-          </p>
-          <p className="cursor-pointer" onClick={handleClick}>
-            لیست قیمت
-          </p>
-          <p
-          className="cursor-pointer"
-            onClick={() => {
-              scrollToSection?.footer();
-              setOpenMenu(false);
-            }}
-          >
-            تماس با ما
-          </p>
-          <Link to="/blogs">
-            <p>بلاگ</p>
-          </Link>
+      {!notNeedMenu && (
+        <div
+          ref={menuRef}
+          className={`w-6/12 h-screen absolute top-0 right-0 bg-[#1D1818] text-white transform transition-transform duration-300 ${
+            openMenu ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <div className="w-10/12 mx-auto flex flex-col gap-4 py-6">
+            <p
+              onClick={() => {
+                scrollToSection?.work();
+                setOpenMenu(false);
+              }}
+            >
+              نمونه کارها
+            </p>
+            <p className="cursor-pointer" onClick={handleClick}>
+              لیست قیمت
+            </p>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                scrollToSection?.footer();
+                setOpenMenu(false);
+              }}
+            >
+              تماس با ما
+            </p>
+            <Link to="/blogs">
+              <p>بلاگ</p>
+            </Link>
+          </div>
         </div>
-      </div>
-        }
+      )}
       {openModal && (
         <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50">
           <div className="inset-0 absolute backdrop-blur-sm"></div>
